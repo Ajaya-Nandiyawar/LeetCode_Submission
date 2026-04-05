dbda99a958d7f5e68c2e4258e1057c67c217e827
Ajaya N <ajaynandiyawar5621@gmail.com>
2026-04-05 16:07:32 +0530

    Time: 1 ms (29.89%) | Memory: 20.5 MB (60.72%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int jump(vector<int>& a) {
+        int jumps = 0, end = 0, far = 0;
+        for (int i = 0; i < a.size() - 1; i++) {
+            far = max(far, i + a[i]);
+            if (i == end) {
+                jumps++;
+                end = far;
+            }
+        }
+        return jumps;
+    }
+};
