aef288f1541e753b4bb17e7df2ec5c188d0bef02
Ajaya N <ajaynandiyawar5621@gmail.com>
2026-04-13 10:31:27 +0530

    Time: 0 ms (100.00%) | Memory: 115.6 MB (67.36%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX, second = INT_MAX;
+        for (int val : nums) {
+            if (val <= first) first = val;
+            else if (val <= second) second = val;
+            else return true;
+        }
+        return false;
+    }
+};
