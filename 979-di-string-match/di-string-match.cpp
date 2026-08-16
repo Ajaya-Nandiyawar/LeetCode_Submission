983aa5d364e7f235c1b0b713f0e82cd8b7a173c5
Ajaya N <ajaynandiyawar5621@gmail.com>
2026-08-16 12:51:46 +0530

    Time: 3 ms (15.23%) | Memory: 11.9 MB (36.69%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int l = 0;
+        int h = s.size();
+        vector<int> ans;
+
+        for (char ch : s) {
+            if (ch == 'I')
+                ans.push_back(l++);
+            else
+                ans.push_back(h--);
+        }
+
+        ans.push_back(l);
+
+        return ans;
+    }
+};
